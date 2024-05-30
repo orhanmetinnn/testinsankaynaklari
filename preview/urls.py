c7257504a420ca774b5ef425a786ff932a79e7b0
orhanmetinnn <orhanmetinnn@hotmail.com>
2024-05-30 16:41:59 +0300

    30.05.2024

--- preview/urls.py
+++ preview/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('insankaynaklari/<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
--- preview/views.py
+++ preview/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 
-
-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
+
 
 def index(request):
-  template = loader.get_template('html/index.html')
-  return HttpResponse(template.render())
+    template = loader.get_template('html/index.html')
+    return HttpResponse(template.render())
+
+def blog_detail(request, slug):
+    post = get_object_or_404(BlogPost, slug=slug)
+    return render(request, 'html/iktree.html', {'post': post})
